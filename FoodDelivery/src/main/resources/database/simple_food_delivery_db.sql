58b00aed7981f79290d34b85e1c1c7a076f091a7
NguyenVan-Trang <trangmoi1911@gmail.com>
2025-05-02 21:40:58 +0700

    sua ten file

--- FoodDelivery/src/main/resources/database/simple_food_delivery_db.sql
+++ FoodDelivery/src/main/resources/database/simple_food_delivery_db.sql
@@ -1,6 +1,6 @@
 
---CREATE DATABASE IF NOT EXISTS `simple_food_delivery_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---USE `simple_food_delivery_db`;
+CREATE DATABASE IF NOT EXISTS `simple_food_delivery_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `simple_food_delivery_db`;
 
 
 SET FOREIGN_KEY_CHECKS=0;
